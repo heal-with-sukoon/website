ec384b31aa58bde41aba415c90a132b57b6af261
anushatomar13 <124388209+anushatomar13@users.noreply.github.com>
2024-07-30 00:22:40 +0530

    Update Therapy.jsx

--- src/components/Therapy/Therapy.jsx
+++ src/components/Therapy/Therapy.jsx
@@ -6,7 +6,7 @@ function Therapy() {
         <div className="top-0 left-0 bg-[#1d2129] p-6">
             <div className='flex justify-center items-center '>
                 <img src="/Therapy/i2.svg" alt="img" className='p-10 pr-32' />
-                <div className=' pt-10 font-rocaThin text-6xl text-white text-center '>Try Our Audio Therapy  <div className='mt-5'>& Video Therapy</div>
+                <div className=' pt-10 font-rocaThin text-6xl text-white text-center '>Try Our Audio & <div className='mt-5'>Video Therapy</div>
                 </div>
                 <img src="/Therapy/i4.svg" alt="img" className='pl-20' />
             </div>
