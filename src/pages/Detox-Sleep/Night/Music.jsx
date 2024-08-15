0315dfc91efcdbde5ed28c8b885c9e3c1041e6f7
heal-with-sukoon <healwithsukoon@gmail.com>
2024-08-15 18:13:06 +0530

    Update Music.jsx

--- src/pages/Detox-Sleep/Night/Music.jsx
+++ src/pages/Detox-Sleep/Night/Music.jsx
@@ -33,7 +33,7 @@ function Audio() {
           From Screens to Serene—our guided Sleep Meditations Help you Unplug and Unwind.
         </div>
         <div className="text-xl ml-4 text-white mr-4 md:ml-14 md:mr-20 md:block hidden">
-          Time to unplug, bud! Swap those screens for some serious zen. Our sleep meditations are like a cozy playlist for your mind, taking you from scrolling to snoozing. Let’s vibe, relax, and recharge—because you deserve that dreamy reset!
+          Time to unplug, bud! Swap those screens for some serious zen. Our sleep meditations are like a cozy playlist for your mind, taking you from scrolling to snoozing. Let us vibe, relax, and recharge—because you deserve that dreamy reset!
         <div className="lg:block hidden flex items-center justify-center ml-20 md:ml-52 scale-100 md:scale-125 mt-20">
           <img
             src="/Meditation/Backward2.svg"
